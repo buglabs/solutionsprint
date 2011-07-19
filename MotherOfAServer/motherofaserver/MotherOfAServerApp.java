6b68afd43f246938885c85e5f6925cfed906f296
Richard Boyle <rmichaelboyle@gmail.com>
2011-07-19 13:47:01 -0400

    Adding mb2controller and motherofaserver

---
+++ MotherOfAServer/motherofaserver/MotherOfAServerApp.java
@@ -0,0 +1,168 @@
+package motherofaserver;
+
+import java.net.ServerSocket;
+import java.util.Map;
+import motherbug2.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+
+
+import com.buglabs.application.ServiceTrackerHelper.ManagedRunnable;
+
+public class MotherOfAServerApp implements ManagedRunnable {
+	final int PORT_NUM = 23;
+	ServerSocket sock;
+	ServoServlet servlet;
+	boolean running = true;
+	private List<do_socket> clients;
+	@Override
+	public void run(Map<Object, Object> services) {			
+		System.out.println("[mothersocketserver] Startup");
+		clients = new ArrayList<do_socket>();
+		servlet = (ServoServlet) services.get(ServoServlet.class.getName());
+		try {
+			sock = new ServerSocket(PORT_NUM);
+		} catch (IOException e) {
+			System.err.println("[mothersocketserver] Unable to open server socket, quitting");
+			shutdown();
+			return;
+		}
+		while (running){
+			Socket client = null;
+			try {
+				client = sock.accept();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				clients.add(new do_socket(client));
+			}
+		}
+	}
+
+	@Override
+	public void shutdown() {
+		System.out.println("[mothersocketserver] Shutdown");
+		kill_all_clients();
+		running = false;
+		try {
+			sock.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// TODO Add shutdown code here if necessary.
+	}
+
+	String doCommand(String degree){
+
+		servlet.set(degree);
+		return "Set " + degree;
+	}
+
+	public String parsePkt(String command){
+		if (command.contains("#")){
+			//If we see a pound, assume the content up to that character is garbage
+			//I'm not sure why, but i'm seeing garbage ending in a # when using 'telnet'
+			//under ubuntu 10.11
+			command = command.substring(command.lastIndexOf('#')+1);
+		}
+		System.out.println("[MothabugSocketServer] Incoming Message: "+command);
+		if(isInt(command))
+			return doCommand(Integer.parseInt(command)+"\n");
+		else if (command.contains("random")){
+			Random generator = new Random();
+			return doCommand(generator.nextInt(179)+"\n");
+		} else if (command.contains("q")){
+			return "Goodbye!";
+		} else {
+			return "number please...\r\n";
+		}
+	}
+
+
+	private class do_socket implements Runnable {
+		Thread mythread;
+		Socket client;
+		BufferedReader in;
+		PrintWriter out;
+
+		public do_socket(Socket new_client){
+			client = new_client;
+			mythread = new Thread(this, "serial reader");
+			mythread.start();		
+		}
+		@Override
+		public void run() {
+			String command = null;
+			try {
+				out = new PrintWriter(client.getOutputStream(), true);
+				in = new BufferedReader( new InputStreamReader( client.getInputStream()));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			out.println("Enter a degree between 0 and 180.");
+			out.println("Or enter 'random' for a random degree.");
+			out.println("Type q or 'quit' to end connection.");
+			do {
+				try {
+					command = in.readLine();
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				String response = parsePkt(command);
+				out.print(response);
+				out.flush();
+			} while (!command.contains("q") && running);
+			try {
+				client.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			remove_client(this);
+		}
+
+		public void stop() {
+			mythread.interrupt();
+			try {
+				client.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			remove_client(this);
+		}
+
+	}
+
+	public void kill_all_clients(){
+		for (int i=0;i<clients.size();i++){
+			clients.get(i).stop();
+		}
+	}
+
+	public void remove_client(do_socket client){
+		clients.remove(client);
+	}
+	public boolean isInt(String i)
+	{
+		try
+		{
+			Integer.parseInt(i);
+			return true;
+		}
+		catch(NumberFormatException nfe)
+		{
+			return false;
+		}
+	}
+}
---
+++ mb2controller/mb2controller/mb2ControlApp.java
@@ -0,0 +1,198 @@
+package mb2controller;
+
+import gnu.io.CommPortIdentifier;
+import gnu.io.NoSuchPortException;
+import gnu.io.PortInUseException;
+import gnu.io.SerialPort;
+
+
+import java.util.Map;
+import java.io.*;
+import java.net.*;
+
+
+import com.buglabs.application.ServiceTrackerHelper.ManagedRunnable;
+
+public class mb2ControlApp implements ManagedRunnable {
+	private SerialPort port;
+	private CommPortIdentifier comm;
+	private InputStream is;
+	private OutputStream os;
+	private PrintWriter out;
+	private Serial_reader reader;
+	private boolean shutdown = false;
+	Socket echoSocket = null;
+	private PrintWriter out2 = null;
+	private BufferedReader in = null;
+
+	@Override
+	public void run(Map<Object, Object> services) {
+		// TODO Auto-generated method stub
+		System.out.println("[Bugduino] service start.");
+		System.out.println("[Bugduino] Attempting to open arduino serial port. ");
+		while (!connectPort()){
+			if (shutdown)
+				return;
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				return;
+			}
+		}
+		System.out.println("[Bugduino] Serial port initialized.");
+		try {
+			reader = new Serial_reader();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void shutdown() {
+		// TODO Auto-generated method stub
+		shutdown = true;
+		reader.stop();
+		out.write("Q\r\n");
+		out.flush();
+		try {
+			is.close();
+			os.close();
+			port.close();
+			out2.close();
+			in.close();
+			echoSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("[Bugduino] service stopped.");
+
+	}
+	private boolean connectPort(){
+		try {
+			System.out.println("try connect port");
+			comm = CommPortIdentifier.getPortIdentifier("/dev/ttyBMI2");
+		} catch (NoSuchPortException e1) {
+			System.out.println("[Bugduino] Port cannot be found");
+			e1.printStackTrace();
+			return false;
+		}
+		try {
+			System.out.println("try 2");
+			port = (SerialPort)comm.open("bugduino", 1000);
+			port.setSerialPortParams(115200, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+			port.setFlowControlMode(SerialPort.FLOWCONTROL_NONE);
+			is = port.getInputStream();
+			os = port.getOutputStream();
+			out = new PrintWriter(os, true);
+
+
+		} catch (PortInUseException e2) {
+			System.err.println("[Bugduino] port in use");
+			return false;
+		} catch (IOException e2) {
+			System.err.println("[Bugduino] Can't get input stream");
+			return false;
+		} catch (Exception e){
+			System.err.println("[Bugduino] Unknown error!");
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	private class Serial_reader implements Runnable {
+		boolean running = true;
+		Thread mythread;
+		InputStreamReader istr;
+		BufferedReader br;
+		
+
+		public Serial_reader() throws IOException, InterruptedException{
+			istr = new InputStreamReader(is);
+			br = new BufferedReader(istr);
+			mythread = new Thread(this, "serial reader");
+			mythread.start();
+			
+			try {
+				System.out.println("[Bugsocket] Trying to connect to motherbug2");
+				echoSocket = new Socket("192.168.20.221", 23);
+				out2 = new PrintWriter(echoSocket.getOutputStream(), true);
+				in = new BufferedReader(new InputStreamReader(
+						echoSocket.getInputStream()));
+			} catch (UnknownHostException e) {
+				System.err.println("Don't know about host: motherbug2.local.");
+				System.exit(1);
+			} catch (IOException e) {
+				System.err.println("Couldn't get I/O for "
+						+ "the connection to: motherbug2.");
+				System.exit(1);
+			}
+			System.out.println("[Bugsocket] Connected to motherbug2");
+			//String line = in.readLine();
+			//while(line!=null)
+			//{
+			//	System.out.println("echo: "+ line);
+			//	line = in;
+			//}
+			
+		}
+		@Override
+		public void run() {
+			String line;
+
+			while (running){
+
+				try {
+					line = br.readLine();
+				} catch (Exception e) {
+					System.err.println("Read thread exception - closing");
+					running = false;
+					return;
+				}
+				if (line.length() < 2)
+					continue;
+				if (line.charAt(0) == 0x02){
+					try {
+						parsepkt(line);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				} else {
+					out2.println(line);
+					if(line.contains("q"))
+						shutdown();
+					out2.flush();
+					try {
+						System.out.println("echo: "+ in.readLine());
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+
+			}
+		}
+		public void stop() {
+			//This will not work if the read is blocking
+			//the input stream needs to be closed to force the read
+			//to throw an IOException
+			//For now, we shall assume that closing the app will do this.
+			running = false;
+		}
+
+
+	}
+	private void parsepkt(String line) throws IOException{
+		line = line.substring(1);
+		System.out.println(line);
+	//	out2.println(line + "\n");
+	//	System.out.println("echo: " + in.readLine());
+	}
+
+}
